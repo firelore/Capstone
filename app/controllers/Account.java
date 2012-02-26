2c6b359c67e6aa0cf0b5d29d3e7fe39410230994
Mike Spencer <firelore@gmail.com>
2012-02-26 10:26:21 -0600

    Initial create account form

---
+++ app/controllers/Account.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+import models.concerts.*;
+
+public class Account extends Controller {
+	public static void create() {
+		render();
+	}
+
+	public static void createAccount() {
+
+	}
+}
